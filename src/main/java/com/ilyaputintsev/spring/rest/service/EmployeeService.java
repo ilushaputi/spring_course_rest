3b9dffa63be130e48aab5ce1c2fa95a8a39ceeff
ilushaputi <putintzeff.ilya@yandex.com>
2022-04-07 19:57:40 +0300

    Spring MVC REST Lesson 7 get List<Employee>

---
+++ src/main/java/com/ilyaputintsev/spring/rest/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.ilyaputintsev.spring.rest.service;
+
+import com.ilyaputintsev.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+    public void deleteEmployee(int id);
+}
